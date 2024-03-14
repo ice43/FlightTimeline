84651adc81b29590764615dd5d02db41ef9ba7e1
bowski43 <43mngt@gmail.com>
2024-03-14 10:49:36 +0300

    added UIKit-framework to swiftUI project

---
+++ FlightTimeline/TimelineView.swift
@@ -0,0 +1,84 @@
+//
+//  TimelineView.swift
+//  FlightTimeline
+//
+//  Created by Serge Broski on 3/14/24.
+//
+
+import SwiftUI
+
+struct TimelineView: UIViewControllerRepresentable {
+    let flights: [FlightInformation]
+    
+    func makeUIViewController(context: Context) -> UITableViewController {
+        UITableViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: UITableViewController, context: Context) {
+        let timelineTableViewCell = UINib(nibName: "TimelineTableViewCell", bundle: nil)
+        
+        uiViewController.tableView.register(
+            timelineTableViewCell,
+            forCellReuseIdentifier: "TimelineTableViewCell"
+        )
+        
+        uiViewController.tableView.dataSource = context.coordinator
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(flights: flights)
+    }
+}
+
+extension TimelineView {
+    final class Coordinator: NSObject, UITableViewDataSource {
+        let flights: [FlightInformation]
+        
+        init(flights: [FlightInformation]) {
+            self.flights = flights
+        }
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            flights.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let dateFormatter = DateFormatter()
+            dateFormatter.timeStyle = .short
+            dateFormatter.dateStyle = .none
+            
+            let flight = flights[indexPath.row]
+            
+            // Запланированные рейсы
+            let scheduledString = dateFormatter.string(from: flight.scheduledTime)
+            // Текущие рейсы
+            let currentString = dateFormatter.string(from: flight.currentTime ?? flight.scheduledTime)
+            
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "TimelineTableViewCell", for: indexPath) as? TimelineTableViewCell else { return UITableViewCell() }
+            
+            var flightInfo = "\(flight.airline) \(flight.number) "
+            flightInfo += "\(flight.direction == .departure ? "to" : "from")"
+            flightInfo += " \(flight.otherAirport) - \(flight.flightStatus)"
+            
+            cell.descriptionLabel.text = flightInfo
+            cell.titleLabel.text = "On Time for \(scheduledString)"
+            
+            if flight.status == .cancelled {
+                cell.titleLabel.text = "Cancelled"
+            } else if flight.timeDifference != 0, flight.status == .cancelled {
+                cell.titleLabel.text = "Cancelled"
+            } else if flight.timeDifference != 0 {
+                cell.titleLabel.text = "\(scheduledString)  Now: \(currentString)"
+            }
+            
+            cell.titleLabel.textColor = .black
+            cell.bubbleColor = flight.timelineColor
+            
+            return cell
+        }
+    }
+}
+
+#Preview {
+    TimelineView(flights: FlightInformation.generateFlights())
+}
