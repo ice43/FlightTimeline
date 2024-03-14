9ed1cb3399a8ab1e1b97ab2a194e90cd4c9b5560
bowski43 <43mngt@gmail.com>
2024-03-14 11:20:16 +0300

    home view has been implemented

--- FlightTimeline/HomeView.swift
+++ FlightTimeline/HomeView.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct HomeView: View {
+    private let flights = FlightInformation.generateFlights()
+    
+    private var arrivals: [FlightInformation] {
+        flights.filter { $0.direction == .arrival }
+    }
+    
+    private var departures: [FlightInformation] {
+        flights.filter { $0.direction == .departure }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ZStack {
+                Image(systemName: "airplane")
+                    .resizable()
+                    .frame(width: 250, height: 250)
+                    .opacity(0.1)
+                    .rotationEffect(.degrees(-90))
+                
+                VStack(alignment: .leading, spacing: 10) {
+                    NavigationLink("Arrivals") {
+                        FlightBoardView(title: "Arrivals", flights: arrivals)
+                    }
+                    
+                    NavigationLink("Departures") {
+                        FlightBoardView(title: "Departures", flights: departures)
+                    }
+                    
+                    NavigationLink("Flight Timeline") {
+                        TimelineView(flights: flights)
+                    }
+                    
+                    Spacer()
+                }
+                .font(.title)
+                .padding()
+            }
+            .navigationTitle("Airport")
+        }
     }
 }
 
