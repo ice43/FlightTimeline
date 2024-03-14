461050c3ee02c17567eb78b6ea265bb171f0aeaf
bowski43 <43mngt@gmail.com>
2024-03-14 10:27:12 +0300

    Initial Commit

---
+++ FlightTimeline/FlightTimelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightTimelineApp.swift
+//  FlightTimeline
+//
+//  Created by Serge Broski on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct FlightTimelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
